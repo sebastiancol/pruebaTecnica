db9c0ef76ac38eea96d747471bbd6852345296a0
Sebastian <sebastian1475@hotmail.com>
2022-11-07 18:17:06 -0500

    cambios en el slider de imagenes y maquetacion

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,38 +28,65 @@ function dataApi(data){
 
     console.log(data)
 
+    //aca traemos el valor del nombre
     const name = document.getElementById('title');
     name.append(data.title)
 
+    //aca traemos el valor del precio
     const price = document.getElementById('price');
     price.append(data.price)
 
+    //aca traemos el precio maximo
     const price_max = document.getElementById('preciHigh');
     price_max.append(data.price_max)
 
+    //aca traemos el valor de la descipcion del producto
     const description = document.getElementById('description');
     description.innerHTML = `<p>${data.description} </p>`
     
+    //aca traemos las imagenes
     const fotos = data.images
     //console.log(fotos)
-    fotos.forEach(f => {
+    let photos = [];
+    photos.push(fotos)
+
+    for(let i in photos){
+        const img= document.createElement('img');
+        img.src=photos[i][0]
+        img.alt="imagen1"
+        const img2= document.createElement('img');
+        img2.src=photos[i][1]
+        const img3 = document.createElement('img');
+        img3.src=photos[i][2]
+        const img4 = document.createElement('img');
+        img4.src=photos[i][3]
+        document.getElementById('images').append(img,img2,img3,img4)
+    }
+
+    /*fotos.forEach(f => {
        
         const img= document.createElement('img');
-        img.src=f.images;                   
+      
+            img.src=f.images;   
+        
+                        
         document.getElementById('images').append(img)
         
-    });
+    });*/
 
-    //<p>${data.variants}</p>
-
+ 
+    //aca traemos la informacion de las variantes del producto
     const variants = data.variants
-    let info= []
+    let varianst_data= []
     variants.forEach(f =>{
         const options= document.createElement('option')
         options.value=f.options
-        info.push(options)
-        //const option2= document.createElement('option')
-        ///option2.value=f.option2
+        varianst_data.push(options)
+       for(let i in varianst_data){
+         //const variant = varianst_data[i][0]
+         //const size = varianst_data[i][1]
+         console.log(varianst_data[i])
+       }
         document.getElementById('options').appendChild(options);
     })
     //console.log(variants)
@@ -98,38 +125,55 @@ function errorModal(){
     console.log('error en el modal')
 }*/
 
+//acciones de la ventana modal
+if(document.getElementById("btnModal")){
+    let modal2 = document.getElementById("myModal");
+    let btn = document.getElementById("btnModal");
+    let span = document.getElementsByClassName("close")[0];
+    let body = document.getElementsByTagName("body")[0];
 
-    if(document.getElementById("btnModal")){
-        let modal2 = document.getElementById("myModal");
-        let btn = document.getElementById("btnModal");
-        let span = document.getElementsByClassName("close")[0];
-        let body = document.getElementsByTagName("body")[0];
-    
-        btn.onclick = function() {
-            modal2.style.display = "block";
-    
-            body.style.position = "static";
-            body.style.height = "100%";
-            body.style.overflow = "hidden";
-        }
-    
-        span.onclick = function() {
+    btn.onclick = function() {
+        modal2.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal2.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal2) {
             modal2.style.display = "none";
-    
+
             body.style.position = "inherit";
             body.style.height = "auto";
             body.style.overflow = "visible";
         }
-    
-        window.onclick = function(event) {
-            if (event.target == modal2) {
-                modal2.style.display = "none";
-    
-                body.style.position = "inherit";
-                body.style.height = "auto";
-                body.style.overflow = "visible";
-            }
-        }
     }
+}
 
+//aca esta la funcionalida del carrosuel
+const main = document.querySelector('.main')
+const point = document.querySelectorAll('.point')
+
+point.forEach( (eachPoint,i)=>{
+    point[i].addEventListener('click',()=>{
+        let position =i;
+        let operation = position * -25
+
+        main.style.transform = `translateX(${ operation}%)`
+
+        point.forEach((eachPoint,i)=>{
+            point[i].classList.remove('active');
+        })
+        point[i].classList.add('active');
+    })
+} );
 
