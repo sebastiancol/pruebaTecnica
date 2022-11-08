ff4f8af4be5b08734c9adb9a907cc0fa1942c54d
Sebastian <sebastian1475@hotmail.com>
2022-11-08 00:20:55 -0500

    cambios en la logica del codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,31 @@
-window.addEventListener('load', () =>{
-    
-    apiRequest()
-});
+'use strict'
 
-async function apiRequest(){
+window.onload = () => {
+    apiRequest();
+}
+
+function apiRequest() {
     //console.log('entrando')
     // en este funcion usamos fetch para enlazar la api
-    await fetch('https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js') 
-    .then(Response=>Response.json())
-    .then(data=>{
-       dataApi(data);
-    })
-    .catch(errorApi)
+    fetch('https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js')
+        .then(response => response.json())
+        .then(data => {
+            dataApi(data);
+        })
+        .catch(errorApi)
 }
 
 
-function dataApi(data){
+function dataApi(data) {
 
     //en esta funcion damos manejos a los datos obtenidos de la api
-    
+
     /* ok nombre
     okprecio real
-    ok precio mas alto %   
+    ok precio mas alto %
     ok descripcion
     ok galeria de imagenes
     ok variantes del prpoducto (s/m/l) y su respectivo precio*/
-
-    console.log(data)
 
     //aca traemos el valor del nombre
     const name = document.getElementById('title');
@@ -43,62 +42,39 @@ function dataApi(data){
     //aca traemos el valor de la descipcion del producto
     const description = document.getElementById('description');
     description.innerHTML = `<p>${data.description} </p>`
-    
-    //aca traemos las imagenes
-    const fotos = data.images
-    //console.log(fotos)
-    let photos = [];
-    photos.push(fotos)
 
-    for(let i in photos){
-        const img= document.createElement('img');
-        img.src=photos[i][0]
-        img.alt="imagen1"
-        const img2= document.createElement('img');
-        img2.src=photos[i][1]
-        const img3 = document.createElement('img');
-        img3.src=photos[i][2]
-        const img4 = document.createElement('img');
-        img4.src=photos[i][3]
-        document.getElementById('images').append(img,img2,img3,img4)
-    }
+    const images = data.media.map(m => m.src);
+    let imageTemplate = '';
 
-    /*fotos.forEach(f => {
-       
-        const img= document.createElement('img');
-      
-            img.src=f.images;   
-        
-                        
-        document.getElementById('images').append(img)
-        
-    });*/
+    images.forEach(url => {
+        imageTemplate += `
+            <img  src='${url}'/>
+        `;
+    });
 
- 
+    document.getElementById('images').innerHTML = imageTemplate;
+
+
     //aca traemos la informacion de las variantes del producto
-    const variants = data.variants
-    let varianst_data= []
-    variants.forEach(f =>{
-        const options= document.createElement('option')
-        options.value=f.options
-        varianst_data.push(options)
-       for(let i in varianst_data){
-         //const variant = varianst_data[i][0]
-         //const size = varianst_data[i][1]
-         console.log(varianst_data[i])
-       }
-        document.getElementById('options').appendChild(options);
-    })
-    //console.log(variants)
+    const variants = data.variants;
+    let variantsTemplate = '';
 
-      
-    
+    variants.forEach(f => {
+        variantsTemplate += `
+            <option value=${f.id}>${f.name}</option>
+        `;
+    });
+
+    document.getElementById('options').innerHTML = variantsTemplate;
+
+
+
 }
 
-function errorApi(){
+function errorApi(err) {
     //funcionn de error si la api falla
-    console.log('error en la api')
-}  
+    console.error('error en la api: ', err)
+}
 
 /*
 const variants = document.getElementById('variants');
@@ -126,13 +102,13 @@ function errorModal(){
 }*/
 
 //acciones de la ventana modal
-if(document.getElementById("btnModal")){
+if (document.getElementById("btnModal")) {
     let modal2 = document.getElementById("myModal");
     let btn = document.getElementById("btnModal");
     let span = document.getElementsByClassName("close")[0];
     let body = document.getElementsByTagName("body")[0];
 
-    btn.onclick = function() {
+    btn.onclick = function () {
         modal2.style.display = "block";
 
         body.style.position = "static";
@@ -140,7 +116,7 @@ if(document.getElementById("btnModal")){
         body.style.overflow = "hidden";
     }
 
-    span.onclick = function() {
+    span.onclick = function () {
         modal2.style.display = "none";
 
         body.style.position = "inherit";
@@ -148,7 +124,7 @@ if(document.getElementById("btnModal")){
         body.style.overflow = "visible";
     }
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target == modal2) {
             modal2.style.display = "none";
 
@@ -163,17 +139,17 @@ if(document.getElementById("btnModal")){
 const main = document.querySelector('.main')
 const point = document.querySelectorAll('.point')
 
-point.forEach( (eachPoint,i)=>{
-    point[i].addEventListener('click',()=>{
-        let position =i;
+point.forEach((eachPoint, i) => {
+    point[i].addEventListener('click', () => {
+        let position = i;
         let operation = position * -25
 
-        main.style.transform = `translateX(${ operation}%)`
+        main.style.transform = `translateX(${operation}%)`
 
-        point.forEach((eachPoint,i)=>{
+        point.forEach((eachPoint, i) => {
             point[i].classList.remove('active');
         })
         point[i].classList.add('active');
     })
-} );
+});
 
